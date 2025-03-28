153a2c710d7cd9e5bd535760688eaa9f565bfca8
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-28 15:10:33 +0100

    feat: importato  e richimato router

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-
+// router
+const postsRouter = require('./routers/postsRouter.js')
 
 // accesso per il client
 app.use(express.static('public'))
 
-
-
+// utilizzo router
+app.use('/posts', postsRouter)
 
 // server in attesa
 app.listen(port,()=>{
